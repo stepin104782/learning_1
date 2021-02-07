fcb740e0f0a27942e4bb05218a29c7c397f33b1f
stepin104782 <69045687+stepin104782@users.noreply.github.com>
2021-02-07 11:29:34 +0530

    Update learning_1.c

--- src/learning_1.c
+++ src/learning_1.c
@@ -1,10 +1,11 @@
 #include"learning_1_header.h"
-
+#include<stdio.h>
 int addition(int a, int b)
 {
     int sum;
     sum = a + b;
-   printf("sum = %d",sum);
+   
+    return(sum)
 
 }
 int main()
@@ -12,7 +13,7 @@ int main()
     int x,y,sum=0;
     printf("enter the two numbers to be added: \n");
     scanf("%d\n%d",&x,&y);
-    addition(x,y);
-
+    
+    printf("sum = %d",addition(x,y));
     return 0;
 }
